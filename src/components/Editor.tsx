2e0e75d963265ad14b8c8948fd645f5749fcc77b
Jacob Handwerger <jstudyeth@gmail.com>
2023-08-30 10:36:52 -0400

    editor work

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,30 +1,45 @@
 import { useState } from "react";
 import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-html";
+import "ace-builds/src-noconflict/mode-css";
+import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/mode-typescript";
 import "ace-builds/src-noconflict/theme-monokai";
 import "ace-builds/src-noconflict/ext-language_tools";
 
 const Editor = () => {
-    const [ code, setCode ] = useState('');
-    const handleChange = () => {
-        console.log(AceEditor)
-    }
+  const [language, setLanguage] = useState("");
+  const [code, setCode] = useState("");
+
+  const handleCodeChange = (newCode: string) => {
+    setCode(newCode);
+  };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("LANGUAGE: ", language);
+  }
   return (
-    <AceEditor
-      mode="typescript"
-      theme="monokai"
-      onChange={()=>console.log()}
-    //   onPaste={()=>setCode()}
-      value=''
-      name="UNIQUE_ID_OF_DIV"
-      editorProps={{ $blockScrolling: true }}
-      enableSnippets={true}
-      enableBasicAutocompletion
-      enableLiveAutocompletion
-      placeholder='Write your code here:'
-      highlightActiveLine
-      fontSize={15}
-    />
+    <>
+    <form onSubmit={handleSubmit}>
+        <input type="text" name="language" placeholder="Enter Language ..." onChange={(e) => setLanguage(e.target.value)}></input>
+        <button type="submit">Enter</button>
+    </form>
+      <AceEditor
+        mode={language}
+        theme="monokai"
+        onChange={handleCodeChange}
+        //   onPaste={()=>setCode()}
+        value={code}
+        name="UNIQUE_ID_OF_DIV"
+        editorProps={{ $blockScrolling: true }}
+        enableSnippets={true}
+        enableBasicAutocompletion
+        enableLiveAutocompletion
+        placeholder="Write your code here:"
+        highlightActiveLine
+        fontSize={15}
+      />
+    </>
   );
 };
 
